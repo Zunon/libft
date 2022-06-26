29143bdcc7d489a3f191e3210ddae1f1d8ae53b6
kalmheir <zunon@zunon.io>
2022-06-26 15:24:43 +0400

    Comment linked list functions

--- src/data_structures/linked_list/linked_list_funcs1.c
+++ src/data_structures/linked_list/linked_list_funcs1.c
@@ -6,12 +6,18 @@
 /*   By: kalmheir <kalmheir@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 12:50:07 by kalmheir          #+#    #+#             */
-/*   Updated: 2022/06/25 12:51:17 by kalmheir         ###   ########.fr       */
+/*   Updated: 2022/06/26 15:24:29 by kalmheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../libft.h"
 
+/**
+ * @brief Finds the tail of the given list.
+ *
+ * @param lst	List to be iterated through.
+ * @return The ultimate node within the list.
+ */
 t_list	*ft_lstlast(t_list *lst)
 {
 	if (!lst)
@@ -21,6 +27,13 @@ t_list	*ft_lstlast(t_list *lst)
 	return (lst);
 }
 
+/**
+ * @brief Unallocates given node.
+ * Uses the given anonymous function del that is supposed to unallocate the
+ * content where applicable.
+ * @param lst	Node to be freed
+ * @param del	Function to free the content of the node
+ */
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (del)
@@ -28,6 +41,12 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	free(lst);
 }
 
+/**
+ * @brief Unallocates the entire list given.
+ * Iterates through the entire list freeing every node using ft_lstdelone().
+ * @param lst	List to be freed
+ * @param del	Function to free the content of a given node.
+ */
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*holder;
@@ -44,6 +63,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	*lst = 0;
 }
 
+/**
+ * @brief Attaches a node to become the new head.
+ *
+ * @param lst List to be attached to.
+ * @param new New head of the list.
+ */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst)
