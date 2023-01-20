e06f5b67dd91abe0c5a26f371cfa9ca4763b0a08
kalmheir <zunon@zunon.io>
2023-01-21 01:56:45 +0400

    new function join_and_free

--- libft.h
+++ libft.h
@@ -145,4 +145,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+char	*join_and_free(char *left, char *right, t_bool fl, t_bool fr);
 #endif
